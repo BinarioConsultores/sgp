a2d1531717cfa4ae951d426bbcf521850bbc065d
DeyvyMY <odr5_deivioscar@hotmail.com>
2019-01-24 08:58:25 -0500

    se arreglo vista lista de recursos de proyecto

--- resources/views/gerente/proyecto/ver.blade.php
+++ resources/views/gerente/proyecto/ver.blade.php
@@ -667,6 +667,7 @@
                   <thead>
                      <tr>
                         <th>Cod.</th>
+                        <th>Period</th>
                         <th>Insumo</th>
                         <th>Unidad</th>
                         <th>Cantidad</th>
@@ -680,14 +681,12 @@
                   <tbody>
                      <?php $catego="";?>
                      @foreach($proyecto->Curs->first()->CurDetalles as $CurDetalle)
+                        
+                        
                         @if($CurDetalle->curd_idpadre!=1)
-                        @if($catego != $CurDetalle->CurdPadre->RecursoUnidadMedida->Recurso->rec_nom)
-                           <tr>
-                              <td class="bg-success"><div class="font-weight-bold">{{$CurDetalle->CurdPadre->RecursoUnidadMedida->Recurso->rec_nom}}</div></td>
-                           </tr>
-                        @endif
                         <tr>
                            <td>{{str_replace(" ","_",$CurDetalle->RecursoUnidadMedida->Recurso->rec_cod)}}</td>
+                           <td>{{$CurDetalle->CurdPadre->RecursoUnidadMedida->Recurso->rec_nom}}</td>
                            <td>
                               <div class="font-weight-semibold">{{$CurDetalle->RecursoUnidadMedida->Recurso->rec_nom}}</div>
                            </td>
@@ -708,7 +707,7 @@
                                  <div class="font-weight-semibold">S/. {{$objCurdPlazo->curdp_cant}}</div>
                               </td>
                            @endforeach
-                           <?php $catego=$CurDetalle->CurdPadre->RecursoUnidadMedida->Recurso->rec_nom;?>
+                           
                         </tr>
                         @endif
                      @endforeach
